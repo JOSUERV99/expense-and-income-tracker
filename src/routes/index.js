aa2a9ec145db372fba5be0ac3b8561954f01ffa4
josuerv99 <josuerojasvega@gmail.com>
2020-12-17 17:51:31 -0600

    Expenses&IncomeTracker: some input validations and cleaning

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const { check, validationResult } = require('express-validator');
+const alert = require('alert');
 const router = Router();
 
 const fireadmin = require('firebase-admin');
@@ -18,41 +20,50 @@ router.get('/', (req, res) => {
             const expenses = snapshot.val();
 
             let movements = [];
-            for ( let [id, expense] of (Object.entries(expenses || []) )) 
-            {
-                console.log(expense);
+            for ( let [id, expense] of (Object.entries(expenses || []) )) {
                 expense.id = id;
                 expense.date = new Date(expense.date).toLocaleDateString();
                 movements.push(expense);
             }
 
-            for ( let [id, income] of (Object.entries(incomes || []) )) 
-            {
+            for ( let [id, income] of (Object.entries(incomes || []) )) {
                 income.id = id;
                 income.date = new Date(income.date).toLocaleDateString();
                 movements.push(income);
             }
 
-            movements.sort(  (a, b) => a.date - b.date );
+            movements.sort(  (a, b) => b.date - a.date );
         
-            res.render('index', {
-                data : movements
-            });
+            res.render('index', {data : movements});
         });
     }) 
 });
 
-router.post('/add', async (req, res) => {
+router.post('/add', [
+    check('description', 'The description cannot be empty').isLength({min:1, max:50}),
+    check('amount', 'The amount value must be a number').isNumeric()
+],async (req, res) => {
 
-    const { description, amount, category, expense } = req.body;
-    const movement = {
-        description, amount, category,
-        type: expense ? 'Expense' : 'Income',
-        date: new Date().getTime()
-    };
+    let errors = validationResult(req);
+    console.log(errors);
 
-    await db.ref(movement.type).push(movement);
-    res.redirect('/');
+    if (errors.errors.length === 0) {
+        console.log(req.body);
+        const { description, amount, category, type } = req.body;
+        const movement = {
+            description, amount, category, type,
+            date: new Date().getTime()
+        };
+
+        await db.ref(movement.type).push(movement);
+        res.redirect('/');
+    }
+    else {
+        for (let error of errors.errors) {
+            if (error) alert(`${error.msg}`);
+        }
+        res.redirect('/');
+    }
 });
 
 router.get('/delete/:id/:type', (req, res) => {
